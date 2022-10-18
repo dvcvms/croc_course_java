708a4fca5f01dbbcb114dc1a892783075c195d46
Max <tetraedrtrapecia@gmail.com>
2022-10-19 01:18:57 +0300

    REFACTOR first task: -add check incorrect values -add double input values

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -15,6 +15,9 @@ public class Main {
         double l2 = Math.sqrt(Math.pow((c.x - a.x), 2) + Math.pow((c.y - a.y), 2));
         double l3 = Math.sqrt(Math.pow((b.x - c.x), 2) + Math.pow((b.y - c.y), 2));
 
+        if (l1 * l2 * l3 == 0.0)
+            return -100;
+
         double p = (l1 + l2 + l3) / 2;
         double s = Math.sqrt(p * (p - l1) * (p - l2) * (p - l3));
 
@@ -29,22 +32,32 @@ public class Main {
         Point b = new Point();
         Point c = new Point();
 
+        double s;
+
         System.out.print("Введите координату х вершины №1: ");
-        a.x = in.nextInt();
+        a.x = in.nextDouble();
         System.out.print("Введите координату y вершины №1: ");
-        a.y = in.nextInt();
+        a.y = in.nextDouble();
 
         System.out.print("Введите координату х вершины №2: ");
-        b.x = in.nextInt();
+        b.x = in.nextDouble();
         System.out.print("Введите координату y вершины №2: ");
-        b.y = in.nextInt();
+        b.y = in.nextDouble();
 
         System.out.print("Введите координату х вершины №3: ");
-        c.x = in.nextInt();
+        c.x = in.nextDouble();
         System.out.print("Введите координату y вершины №3: ");
-        c.y = in.nextInt();
+        c.y = in.nextDouble();
 
-        System.out.println("Площадь равна: " + String.format("%.1f", Square(a, b, c)));
+        s = Square(a, b, c);
+
+        if (s == -100){
+            System.out.println("Координаты вершин заданы некорректно!");
+        }
+        else {
+            System.out.println("Площадь равна: " + String.format("%.1f", Square(a, b, c)));
+        }
+
         // System.out.format("%.1f", Square(a, b, c) + "Площадь равна");
     }
 }
