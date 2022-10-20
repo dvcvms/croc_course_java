1e994557235ebac5a6a8e9429e711a586e5966a3
Max <tetraedrtrapecia@gmail.com>
2022-10-20 16:20:24 +0300

    ADD task3: -use StringBuffer -done decomposition methods -add explanatory comments

---
+++ src/ru/croc/task3/Task3.java
@@ -0,0 +1,117 @@
+package ru.croc.task3;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void fillArray(int[] arr, String str) {
+        // String t = "";
+        int j = 0;
+
+        StringBuilder buffer = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) != ' ') {
+                // t = t + str.charAt(i);
+                buffer.append(str.charAt(i));
+            }
+
+            if (str.charAt(i) == ' ') {
+                // arr[j] = Integer.parseInt(t);
+                arr[j] = Integer.parseInt(buffer.substring(0));
+                // t = "";
+                j++;
+                buffer.delete(0, buffer.length());
+            }
+
+
+        }
+    }
+
+    public static void findMaxMin(int[] arr) {
+        int lengthArray = arr.length;
+
+        int maxValue = arr[0], minValue = arr[0];
+        int index_min = 0, index_max = 0;
+
+        for (int i = 1; i < lengthArray; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                index_max = i;
+            }
+
+            if (arr[i] < minValue) {
+                minValue = arr[i];
+                index_min = i;
+            }
+        }
+        changeMaxMin(arr, minValue, maxValue, index_min, index_max);
+    }
+
+    public static void changeMaxMin(int[] arr, int minValue, int maxValue, int index_min, int index_max) {
+        int t_min = arr[0];
+        arr[0] = minValue;
+        arr[index_min] = t_min;
+
+        /*
+         * We must check this situation:
+         *
+         * 4 1 2 3 -> 1 4 2 3
+         *
+         * Because maxValue changed position then index_max must be changed:
+         */
+
+        if (arr[index_min] == maxValue) {
+            index_max = index_min;
+        }
+
+        int t_max = arr[arr.length - 1];
+        arr[arr.length - 1] = maxValue;
+        arr[index_max] = t_max;
+    }
+
+    public static void printArray(int[] arr) {
+        int lengthArray = arr.length;
+
+        System.out.print(arr[0]);
+        for (int i = 1; i < lengthArray; i++) {
+            System.out.print(" " + arr[i]);
+        }
+    }
+
+    public static int findLengthArray(String str) {
+        int lengthArray = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                lengthArray++;
+            }
+        }
+
+        return lengthArray;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int lengthArray;
+        String str;
+
+        str = in.nextLine();
+        str = str + " ";
+
+        lengthArray = findLengthArray(str);
+
+        int[] arr = new int[lengthArray];
+
+        fillArray(arr, str);
+
+        findMaxMin(arr);
+
+        findMaxMin(arr);
+
+        printArray(arr);
+    }
+
+}
