397d7b0fe0e552115856c2e08135b394817815f5
Max <tetraedrtrapecia@gmail.com>
2022-11-12 17:13:26 +0300

    ADD solution task9

--- src/ru/croc/task9/Task9.java
+++ src/ru/croc/task9/Task9.java
@@ -4,5 +4,10 @@ public class Task9 {
 
     public static void main(String[] args) {
 
+        String path = "././././../././././CROC/work/src/././../.././university/.././../.././././memes/cats/1ph_oto01/./.././";
+
+        Normalization n;
+        System.out.println(Normalization.normalizePath(path));
+
     }
 }
