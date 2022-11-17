c0f967a358de11871ca7935828d1c1c35096c7af
dvcvms <tetraedrtrapecia@gmail.com>
2022-11-17 18:15:20 +0300

    FIX task9: -erroneous deletion of the first character is excluded -work with russian symbols -add tests

--- src/ru/croc/task9/Normalization.java
+++ src/ru/croc/task9/Normalization.java
@@ -27,6 +27,9 @@ public class Normalization {
         // ./otherDirectories/... -> otherDirectories/...
         if (path.charAt(0) == '.' && path.charAt(1) == '/')
             i = 3;
+        else {
+            newStr.append(path.charAt(0));
+        }
 
         while (i < path.length()) {
             if (path.charAt(i - 2) != '.' && path.charAt(i - 1) == '.' && path.charAt(i) == '/') {
@@ -36,6 +39,8 @@ public class Normalization {
                 i++;
             }
         }
+
+        newStr.append(path.charAt(path.length() - 1));
 
         return newStr.toString();
     }
@@ -48,15 +53,14 @@ public class Normalization {
         do {
             prevAnswer = currentAnswer;
 
-            currentAnswer = currentAnswer.replaceAll("[{0-9A-z_\\-}]+[/][.][.][/]", "");
+            currentAnswer = currentAnswer.replaceAll("[{0-9A-zА-я_\\-}]+[/][.][.][/]", "");
 
         } while (prevAnswer.length() != currentAnswer.length());
 
 
-        // .../lastDirectory/ -> .../lastDirectory
-        if (currentAnswer.charAt(currentAnswer.length() - 1) == '/') {
+        // delete slashes in the end of path: .../lastDirectory/ -> .../lastDirectory
+        while (currentAnswer.charAt(currentAnswer.length() - 1) == '/' && currentAnswer.charAt(currentAnswer.length() - 2) != '.')
             currentAnswer = currentAnswer.substring(0, currentAnswer.length() - 1);
-        }
 
         return currentAnswer;
     }
--- src/ru/croc/task9/Task9.java
+++ src/ru/croc/task9/Task9.java
@@ -4,10 +4,56 @@ public class Task9 {
 
     public static void main(String[] args) {
 
-        String path = "././././../././././CROC/work/src/././../.././university/.././../.././././memes/cats/1ph_oto01/./.././";
+        String[] tests = {
+                "../../../CROC/work/src/../../../../../university/../../../../../../../memes/cats/1ph_oto01/../../../",
+                "./././CROC/work/src/./././././university/./././././././memes/cats/1ph_oto01/./././",
+                "CROC/work/src/university/memes/cats/1ph_oto01",
+                "../CROC/work/src/../university/../memes/cats/1ph_oto01/../",
+                "./CROC/work/src/./university/./memes/cats/1ph_oto01/./",
+                "./../CROC/work/src/./../university/./../memes/cats/1ph_oto01/./../",
+                ".././CROC/work/src/.././university/.././memes/cats/1ph_oto01/.././",
+                "КРОК/работа/src/./../../универ/../../../мемы/котики",
+                "../../../КРОК/работа/src/../../../../../university/../../../../../../../мемы/cats/1ph_oto01/../../../",
+                "./././КРОК/работа/src/./././././универ/./././././././мемы/cats/1ph_oto01/./././",
+                "КРОК/работа/src/универ/мемы/cats/1ph_oto01",
+                "../КРОК/работа/src/../универ/../мемы/cats/1ph_oto01/../",
+                "./КРОК/работа/src/./универ/./мемы/cats/1ph_oto01/./",
+                "./../КРОК/работа/src/./../универ/./../мемы/cats/1ph_oto01/./../",
+                ".././КРОК/работа/src/.././универ/.././мемы/cats/1ph_oto01/.././"
+        };
 
-        Normalization n;
-        System.out.println(Normalization.normalizePath(path));
+        String[] answers = {
+                "../../../../../../../../../../../",
+                "CROC/work/src/university/memes/cats/1ph_oto01",
+                "CROC/work/src/university/memes/cats/1ph_oto01",
+                "../CROC/work/memes/cats",
+                "CROC/work/src/university/memes/cats/1ph_oto01",
+                "../CROC/work/memes/cats",
+                "../CROC/work/memes/cats",
+                "../мемы/котики",
+                "../../../../../../../../../../../",
+                "КРОК/работа/src/универ/мемы/cats/1ph_oto01",
+                "КРОК/работа/src/универ/мемы/cats/1ph_oto01",
+                "../КРОК/работа/мемы/cats",
+                "КРОК/работа/src/универ/мемы/cats/1ph_oto01",
+                "../КРОК/работа/мемы/cats",
+                "../КРОК/работа/мемы/cats"
+        };
 
+        for (int i = 0; i < tests.length; i++) {
+            String t = Normalization.normalizePath(tests[i]);
+            boolean b = t.equals(answers[i]);
+
+            if (!b) {
+                System.out.print("Тест " + i + " = ");
+                System.out.println("неправильный:");
+                System.out.println(t);
+                System.out.println(answers[i]);
+                System.out.println();
+            } else {
+                System.out.println("Тест " + i + " - " + b);
+            }
+
+        }
     }
 }
