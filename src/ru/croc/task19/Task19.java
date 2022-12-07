0d984640e03903708261774093a744071c09fead
dvcvms <dvcvms@gmail.com>
2022-12-07 15:33:19 +0300

    ADD solution task19

--- src/ru/croc/task19/Task19.java
+++ src/ru/croc/task19/Task19.java
@@ -1,7 +1,15 @@
 package ru.croc.task19;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Task19 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        try(FileWriter writer = new FileWriter(args[0])) {
+            writer.write("Hello world!");
+            writer.flush();
+        }
 
     }
 }
